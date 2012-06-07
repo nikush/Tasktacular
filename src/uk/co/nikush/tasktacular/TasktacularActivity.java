15a2e59fa8e684031239ac36648184d725f88365
Nikush Patel <nikush.patel7@gmail.com>
2012-06-07 12:24:21 +0100

    Initial commit.

---
+++ src/uk/co/nikush/tasktacular/TasktacularActivity.java
@@ -0,0 +1,13 @@
+package uk.co.nikush.tasktacular;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TasktacularActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
