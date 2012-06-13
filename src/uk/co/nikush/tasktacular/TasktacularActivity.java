66d7c71d8fb31fffa4858a10ae4d7f47ef2e23af
Nikush Patel <nikush.patel7@gmail.com>
2012-06-13 13:52:25 +0100

    Add activity life cycle methods
    To better remember them

--- src/uk/co/nikush/tasktacular/TasktacularActivity.java
+++ src/uk/co/nikush/tasktacular/TasktacularActivity.java
@@ -20,6 +20,7 @@ public class TasktacularActivity extends Activity
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        // The activity is being created.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
@@ -45,6 +46,41 @@ public class TasktacularActivity extends Activity
 
         actionBar.addTab(allTab);
         actionBar.addTab(projectsTab);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        // The activity is about to become visible.
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        // The activity is about to be destroyed.
     }
 
     public boolean onCreateOptionsMenu(Menu menu)
