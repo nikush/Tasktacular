39b74b65f36158101b016442a1a1e4cd8a479769
Nikush Patel <nikush.patel7@gmail.com>
2012-06-18 13:10:14 +0100

    Restructure classes
    Move table creation scripts into database helper class

--- src/uk/co/nikush/tasktacular/database/DatabaseHelper.java
+++ src/uk/co/nikush/tasktacular/database/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package uk.co.nikush.tasktacular.database;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Simple class used to help working with the SQLiteOpenHelper.
@@ -17,11 +19,40 @@ public abstract class DatabaseHelper extends SQLiteOpenHelper
 
     public static final int DATABASE_VERSION = 1;
 
+    public static final String TABLE_CREATE_TASKS = "CREATE TABLE "
+            + TasksTable.TABLE_NAME + " (" + TasksTable.KEY_ROWID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, " + TasksTable.KEY_TITLE
+            + " TEXT NOT NULL, " + TasksTable.KEY_DESCRIPTION + " TEXT, "
+            + TasksTable.KEY_COMPLETE + " TEXT, " + TasksTable.KEY_DATE_CREATED
+            + " TEXT NOT NULL, " + TasksTable.KEY_DATE_DUE + " TEXT, "
+            + TasksTable.KEY_DATE_LAST_MODIFIED + " TEXT NOT NULL);";
+
     protected SQLiteDatabase db;
 
     public DatabaseHelper(Context context)
     {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        try
+        {
+            db.execSQL(TABLE_CREATE_TASKS);
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        Log.w("TasksTable", "Upgrading TASKS from version " + oldVersion
+                + " to " + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TasksTable.TABLE_NAME);
+        onCreate(db);
     }
 
     public void open()
--- src/uk/co/nikush/tasktacular/database/TasksTable.java
+++ src/uk/co/nikush/tasktacular/database/TasksTable.java
@@ -7,8 +7,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 public class TasksTable extends DatabaseHelper
 {
@@ -42,57 +40,11 @@ public class TasksTable extends DatabaseHelper
 
     public static final String TABLE_NAME = "tasks";
 
-    static final String TABLE_CREATE = "CREATE TABLE " + TABLE_NAME + " ("
-            + KEY_ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_TITLE
-            + " TEXT NOT NULL, " + KEY_DESCRIPTION + " TEXT, " + KEY_COMPLETE
-            + " TEXT, " + KEY_DATE_CREATED + " TEXT NOT NULL, " + KEY_DATE_DUE
-            + " TEXT, " + KEY_DATE_LAST_MODIFIED + " TEXT NOT NULL);";
-
     SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public TasksTable(Context ctx)
     {
         super(ctx);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db)
-    {
-        try
-        {
-            db.execSQL(TABLE_CREATE);
-        } catch (SQLException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
-    {
-        Log.w("TasksTable", "Upgrading TASKS from version " + oldVersion
-                + " to " + newVersion + ", which will destroy all old data");
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
-    }
-
-    public long insertTask(String title, String description, String due_date)
-    {
-        ContentValues initialValues = new ContentValues();
-        initialValues.put(KEY_TITLE, title);
-        initialValues.put(KEY_DESCRIPTION, description);
-        initialValues.put(KEY_COMPLETE, 0);
-
-        String now = format.format(new Date());
-        initialValues.put(KEY_DATE_CREATED, now);
-        initialValues.put(KEY_DATE_DUE, due_date);
-        initialValues.put(KEY_DATE_LAST_MODIFIED, now);
-        return db.insert(TABLE_NAME, null, initialValues);
-    }
-
-    public boolean deleteTask(long rowId)
-    {
-        return db.delete(TABLE_NAME, KEY_ROWID + "=" + rowId, null) > 0;
     }
 
     public Cursor getAllTasks()
@@ -113,6 +65,20 @@ public class TasksTable extends DatabaseHelper
         return mCursor;
     }
 
+    public long insertTask(String title, String description, String due_date)
+    {
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_TITLE, title);
+        initialValues.put(KEY_DESCRIPTION, description);
+        initialValues.put(KEY_COMPLETE, 0);
+    
+        String now = format.format(new Date());
+        initialValues.put(KEY_DATE_CREATED, now);
+        initialValues.put(KEY_DATE_DUE, due_date);
+        initialValues.put(KEY_DATE_LAST_MODIFIED, now);
+        return db.insert(TABLE_NAME, null, initialValues);
+    }
+
     public boolean updateTask(long rowId, String title, String description, String due_date)
     {
         String now = format.format(new Date());
@@ -123,6 +89,11 @@ public class TasksTable extends DatabaseHelper
         args.put(KEY_DATE_LAST_MODIFIED, now);
         args.put(KEY_DATE_DUE, due_date);
         return db.update(TABLE_NAME, args, KEY_ROWID + "=" + rowId, null) > 0;
+    }
+
+    public boolean deleteTask(long rowId)
+    {
+        return db.delete(TABLE_NAME, KEY_ROWID + "=" + rowId, null) > 0;
     }
 
     public void markAsComplete(long rowId)
